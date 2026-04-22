e134a9423c772522d8992b2fb25757d6a5347b00
Utso Sarkar <utsosarkar7870@gmail.com>
2026-04-22 16:31:52 +0600

    connection request list page done

--- app/(Frond-End)/_components/mainPage/network/ConnectionRequestSection.tsx
+++ app/(Frond-End)/_components/mainPage/network/ConnectionRequestSection.tsx
@@ -1,6 +1,4 @@
-import Search from "@/components/reusable/Search";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
+import ConnectionRequestHeader from "./connectionRequests/ConnectionRequestHeader";
 import ConnectionRequestList from "./connectionRequests/ConnectionRequestList";
 import SuggestConnectionList from "./connectionUsers/SuggestConnectionList";
 
@@ -11,26 +9,7 @@ function ConnectionRequestSection({
 }) {
   return (
     <section className="w-full bg-white  pb-4 pt-2 md:px-3">
-      <div className="flex flex-wrap items-center justify-between gap-3 border-b border-borderColor pb-2">
-        <div className="flex  items-center gap-3">
-          <h2 className="text-xl font-semibold leading-[120%] text-headerColor">
-            Connection Request{" "}
-            <span className="text-base text-grayColor1 font-normal">(15)</span>
-          </h2>
-
-          <div className="relative w-[300px] hidden md:block  max-w-full">
-            <Search />
-          </div>
-        </div>
-
-        <Link
-          href="/mu/id/connection_requests"
-          className="inline-flex items-center gap-1 text-[15px] font-semibold text-headerColor"
-        >
-          Show All
-          <ArrowRight size={16} />
-        </Link>
-      </div>
+      <ConnectionRequestHeader />
       <div>
         <ConnectionRequestList />
       </div>
--- app/(Frond-End)/mu/[id]/(muGroup)/(home_network)/connection-requests/page.tsx
+++ app/(Frond-End)/mu/[id]/(muGroup)/(home_network)/connection-requests/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import ConnectionRequestHeader from "@/app/(Frond-End)/_components/mainPage/network/connectionRequests/ConnectionRequestHeader";
+import ConnectionRequestList from "@/app/(Frond-End)/_components/mainPage/network/connectionRequests/ConnectionRequestList";
 
 function page() {
   return (
     <div>
-      
+      <ConnectionRequestHeader />
+      <div>
+        <ConnectionRequestList />
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
