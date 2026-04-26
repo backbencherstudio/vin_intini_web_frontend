d682d6a00f968cc408dab97410028584b885151e
Utso Sarkar <utsosarkar7870@gmail.com>
2026-04-26 11:04:53 +0600

    update

--- app/(Frond-End)/mu/[id]/layout.tsx
+++ app/(Frond-End)/mu/[id]/layout.tsx
@@ -2,13 +2,12 @@ import MainFooter from "@/components/reusable/MainFooter";
 import React from "react";
 import MainNavbar from "../../_components/mainPage/MainNavbar";
 
-
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div>
+      <div className=" grid grid-rows-[auto_1fr_auto] min-h-screen">
         <MainNavbar />
-        <div className="container ">{children}</div>
+        <div className="container h-full w-full  ">{children}</div>
         <MainFooter />
       </div>
     </div>
