8b9600c79984e6b8b68b637267891392a1e79c86
backbencherstudio <bbsfullstack@gmail.com>
2026-04-26 11:07:58 +0600

    Update StateDetails.tsx

--- app/(Frond-End)/mu/[id]/(muGroup)/academia/[stateId]/_components/StateDetails.tsx
+++ app/(Frond-End)/mu/[id]/(muGroup)/academia/[stateId]/_components/StateDetails.tsx
@@ -14,9 +14,9 @@ export default function StateDetails({ id }: PropType) {
     return (
         <div style={{ padding: '10px' }}>
             <h3>University Search: {id}</h3>
-            <div className="h-[600px]">
+            {/* <div className="h-[600px]">
                 <GoogleMap state={decodeURIComponent(id).split(" ").join("+")} zoom={6} query="graduate and undergraduate"/>
-            </div>
+            </div> */}
         </div>
     );
 }
