ea14a91da916d745890ada95175802dc0f10b546
Utso Sarkar <utsosarkar7870@gmail.com>
2026-05-05 18:02:48 +0600

    done middleware setup

--- app/(Frond-End)/_components/RootProtectedLayout.tsx
+++ app/(Frond-End)/_components/RootProtectedLayout.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import MainFooter from "@/components/reusable/MainFooter";
-import { CookieHelper } from "@/helper/cookie.helper";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import MainNavbar from "./mainPage/MainNavbar";
 
 export default function RootProtectedLayout({
@@ -11,20 +6,10 @@ export default function RootProtectedLayout({
 }: {
   children: React.ReactNode;
 }) {
-    const searchParams = useSearchParams();
-    const router = useRouter();
-    const token = searchParams.get("auth");
-    useEffect(()=>{
-      if(token){
-        CookieHelper.set({key: "accessToken", value: JSON.parse(atob(token || "")).token});
-        router.push("/mu/home")
-      }
-    },[token])
-
   return (
     <div className=" grid grid-rows-[auto_1fr_auto] min-h-screen">
       <MainNavbar />
-      <div className="container h-full w-full  ">{children}</div>
+      <div className="container h-full w-full ">{children}</div>
       <MainFooter />
     </div>
   );
