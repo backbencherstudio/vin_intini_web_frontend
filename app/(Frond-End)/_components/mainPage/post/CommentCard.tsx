54982308439fe022b42538a5b7fd4bb4fc2405d0
Utso Sarkar <utsosarkar7870@gmail.com>
2026-05-10 10:32:54 +0600

    comment delete done

--- app/(Frond-End)/_components/mainPage/post/CommentCard.tsx
+++ app/(Frond-End)/_components/mainPage/post/CommentCard.tsx
@@ -4,11 +4,13 @@ import {
 } from "@/feature/slice/post/likeSlice";
 import { formatPostDate } from "@/lib/utils";
 import emptyImage from "@/public/empty_user.jpg";
+import { DeleteIcon } from "@/public/svgIcons/Icons";
 import Image from "next/image";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import CommentReplyLikeList from "./CommentReplyLikeList";
 import CommentLikeList from "./ReplyLikeListDialog";
+import CommentDeleteDialog from "./CommentDeleteDialog";
 export default function CommentRow({
   depth = 0,
   showReply = false,
@@ -26,6 +28,7 @@ export default function CommentRow({
 }) {
   const [commentLikePost] = useCommentLikePostMutation();
   const [likeListOpen, setLikeListOpen] = useState(false);
+  const [commentDeleteOpen, setCommentDeleteOpen] = useState(false);
   const [replyLikeListOpen, setReplyLikeListOpen] = useState(false);
   const [replyToggleLikeById] = useReplyToggleLikeByIdMutation();
   const [isLiked, setIsLiked] = useState(Boolean(item?.liked_by_me));
@@ -141,6 +144,12 @@ export default function CommentRow({
               Reply • {item?.replies_count || 0}
             </button>
           </>
+          <>
+            <span className="text-headerColor/45">|</span>
+            <button className="cursor-pointer " onClick={() => setCommentDeleteOpen(true)}>
+              <DeleteIcon className="text-redColor w-4 h-4 " />
+            </button>
+          </>
         </div>
       ) : (
         <div className=" text-descriptionColor  gap-1 flex items-center mt-4 ">
@@ -173,6 +182,15 @@ export default function CommentRow({
           commentId={item?.id}
         />
       )}
+      {
+        commentDeleteOpen && (
+          <CommentDeleteDialog
+            open={commentDeleteOpen}
+            setOpen={setCommentDeleteOpen}
+            commentId={item?.id}
+          />
+        )
+      }
     </div>
   );
 }
