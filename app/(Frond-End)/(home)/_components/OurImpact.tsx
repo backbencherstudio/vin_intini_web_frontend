db96849db05d7a2132f08b2ba3bd6f8671f938e6
Utso Sarkar <utsosarkar7870@gmail.com>
2026-08-02 15:04:39 +0600

    Fix: about page content update

--- app/(Frond-End)/(home)/_components/OurImpact.tsx
+++ app/(Frond-End)/(home)/_components/OurImpact.tsx
@@ -47,7 +47,7 @@ export default function OurImpact() {
                 gradient={false}
                 speed={50}
                 pauseOnHover={true}
-                className="flex group "
+                className=" "
               >
                 {item.map((coin, index) => (
                   <div
@@ -62,9 +62,9 @@ export default function OurImpact() {
                       height={60}
                       className="object-contain"
                     />
-                    <div className="absolute bottom-13 left-1/2 -translate-x-1/4 opacity-0 group-hover:opacity-100 transition-opacity bg-primaryColor text-white text-xs px-2 py-1 rounded">
+                    {/* <div className="absolute bottom-13 left-1/2 -translate-x-1/4 opacity-0 group-hover:opacity-100 transition-opacity bg-primaryColor text-white text-xs px-2 py-1 rounded">
                       {coin.name}
-                    </div>
+                    </div> */}
                   </div>
                 ))}
               </Marquee>
