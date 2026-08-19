f46cf519550a24419d258c7d244421f8190968f1
Utso Sarkar <utsosarkar7870@gmail.com>
2026-08-19 10:24:27 +0600

    Dev: done profile locked

--- app/(Frond-End)/mu/profile/[id]/(profile)/page.tsx
+++ app/(Frond-End)/mu/profile/[id]/(profile)/page.tsx
@@ -1,30 +1,14 @@
-import ProfileAbout from "@/app/(Frond-End)/_components/mainPage/profile/ProfileAbout";
-
-import ProfileEducationList from "@/app/(Frond-End)/_components/mainPage/profile/Education/ProfileEducationList";
-import ProfileExpreance from "@/app/(Frond-End)/_components/mainPage/profile/expreance/ProfileExpreance";
 import ProfileHeroSection from "@/app/(Frond-End)/_components/mainPage/profile/ProfileHeroSection";
-import ProfileLockDesign from "@/app/(Frond-End)/_components/mainPage/profile/ProfileLockDesign";
-import ProfilePostList from "@/app/(Frond-End)/_components/mainPage/profile/ProfilePostList";
+import ProfileLockedContent from "@/app/(Frond-End)/_components/mainPage/profile/ProfileLockedContent";
 
 async function page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
-  const protect = true;
-  const isprofile = false;
+
   return (
     <div className="space-y-6">
-      <ProfileHeroSection userId={id} protect={protect} isprofile={isprofile} />
-      {protect ? (
-        <ProfileLockDesign />
-      ) : (
-        <div>
-          <ProfileAbout userId={id} />
-          <ProfilePostList />
-          <ProfileExpreance userId={id} />
-          <ProfileEducationList userId={id} />
-        </div>
-      )}
+      <ProfileHeroSection userId={id} />
+      <ProfileLockedContent userId={id} />
     </div>
   );
 }
-
 export default page;
