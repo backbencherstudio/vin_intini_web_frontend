f9b4124b51b1e9746ae07c3bcf3e88523a963e10
Utso Sarkar <utsosarkar7870@gmail.com>
2026-08-25 17:13:28 +0600

    dev: update generale setting

---
+++ feature/slice/settingSlice.ts
@@ -0,0 +1,34 @@
+import baseApiSlice from "./baseApi";
+
+const settingSlice = baseApiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+    generalSettingUpdate: builder.mutation({
+            query: ({ data }) => ({
+                url: "/profile/update",
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ["User"],
+        }),
+    generalPrivacySettingUpdate: builder.mutation({
+            query: ({ data }) => ({
+                url: "/profile/update",
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ["User"],
+        }),
+   getAllStates: builder.query({
+            query: () => ({
+                url: "/states", 
+                method: "GET",
+            }),
+            providesTags: ["User"],
+        }),
+    }),
+});
+
+
+
+
+export const { useGeneralSettingUpdateMutation, useGeneralPrivacySettingUpdateMutation, useGetAllStatesQuery } = settingSlice;
